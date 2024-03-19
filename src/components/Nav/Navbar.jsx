bb31b478e5375120c63d4d47ec08faa7208a2752
WojteGK <93033932+WojteGK@users.noreply.github.com>
2024-03-19 16:08:03 +0100

    Add Navbar component and its styles

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
+import '../Nav/Navbar.css'
 
 function Navbar() {
   return (    
    <nav>
-    <div class="nav-wrapper">
-      <h1 class="title black-ops">Wojciech Górecki</h1>
+    <div className="nav-wrapper">
+      <h1 className="title black-ops">Wojciech Górecki</h1>
       <ul>
         <li><a href="#about-me">O mnie</a></li>
-        <li><a href="#what-i-do">Czym się zajmuję</a></li>
-        <li><a href="#what-i-know">Na czym się znam</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#capabilities">Czym się zajmuję</a></li>
+        <li><a href="#skills">Na czym się znam</a></li>
+        <li><a href="#projects">Portfolio</a></li>
+        <li><a href="https://github.com/WojteGK" target='blank'>GitHub</a></li>
       </ul>
     </div>
   </nav>
