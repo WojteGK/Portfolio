af136610667b239313ec32890cd652e6f83ab15f
WojteGK <93033932+WojteGK@users.noreply.github.com>
2024-03-22 11:06:43 +0100

    Update CSS styles and add responsive design for mobile devices

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,17 +1,23 @@
-import React from 'react'
+
+import React, { useState } from 'react';
 import '../Nav/Navbar.css'
 
 function Navbar() {
   return (    
    <nav>
     <div className="nav-wrapper">
+    <button className="hamburger" id="expand-button">
+      <div className="line"></div>
+      <div className="line"></div>
+      <div className="line"></div>
+    </button>
       <h1 className="title black-ops">Wojciech Górecki</h1>
       <ul>
         <li><a href="#about-me">O mnie</a></li>
         <li><a href="#capabilities">Czym się zajmuję</a></li>
         <li><a href="#skills">Na czym się znam</a></li>
         <li><a href="#projects">Portfolio</a></li>
-        <li><a href="https://github.com/WojteGK" target='blank'>GitHub</a></li>
+        <li><a href="https://github.com/WojteGK" target='blank'><i class="fab fa-github"> GitHub</i></a></li>
       </ul>
     </div>
   </nav>
