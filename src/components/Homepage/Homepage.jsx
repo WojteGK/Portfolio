3927de168ae5bee29fbfa1947758e76b627d2038
WojteGK <93033932+WojteGK@users.noreply.github.com>
2024-05-17 16:08:13 +0200

    [fix]: small typo in warning

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -6,7 +6,7 @@ function Homepage() {
     <div className='subpage homepage'>
       <h1>Welcome to my website!</h1>
       <p>Feel free to explore the site and learn more about me and my projects!</p>
-      <p className="development-warning">Please have in mind that this side is still under development :)</p>
+      <p className="development-warning">Please have in mind that this site is still under development :)</p>
     </div>
   )
 }
