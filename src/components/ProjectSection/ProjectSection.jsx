715df9f7e2b4c849a976929a1102d5c064e59c2f
WojteGK <93033932+WojteGK@users.noreply.github.com>
2024-05-17 18:33:24 +0200

    [Add]: Project component and styles for displaying projects

--- src/components/ProjectSection/ProjectSection.jsx
+++ src/components/ProjectSection/ProjectSection.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import './ProjectSection.css'
+import Project from './Project/Project.jsx'
 
 function ProjectSection() {
+  const projects = [
+    { name: 'The Goodnight project', 
+      link: 'https://github.com/WojteGK/GoodNight-Project',
+      description: 'A project developed by me and 3 other students that aimed into creating mobile app that was meant to improve our sleep experience by waking user up after exact calculated time while user was in REM sleep phase. ',
+      imgPath: './icons/project_default.png',
+      imgAlt: 'project image',},
+    { name: 'The ModLoader Project',
+      link: 'https://github.com/WojteGK/Mod-Loader',
+      description: 'A project of none GUI app that aimed into creating a simple modpack installer into Minecraft game. ',
+      imgPath: './icons/project_default.png',
+      imgAlt: 'project image',},
+    { name: 'The Discover your drink project',
+      link: 'https://github.com/Krzysiek742/Discover-your-drink',
+      description: 'A project of a website that aimed into helping user to find a drink that would suit him the best. ',
+      imgPath: 'icons/project_default.png',
+      imgAlt: 'project image',}
+  ]
   return (
     <div id="projects" className='subpage projects'>
       <h2>My projects:</h2>
-      <ol>
-         <li><a href="https://github.com/WojteGK/GoodNight-Project">The Goodnight project</a></li>
-         <li><a href='https://github.com/WojteGK/Mod-Loader'>The ModLoader Project</a></li>
+      <ol className='project-list'>
+        {projects.map((project, index) => (
+          <li key={index}>
+            <Project name={project.name} link={project.link} description={project.description} imgPath={project.imgPath} imgAlt={project.imgAlt}/>
+          </li>
+        ))}
       </ol>
     </div>
   )
