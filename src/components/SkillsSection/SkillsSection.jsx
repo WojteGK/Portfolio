97b37afa267a50e3d0312513a904cc652cd96aa7
WojteGK <93033932+WojteGK@users.noreply.github.com>
2024-07-15 20:27:49 +0200

    [update]: SkillsSection alignment and content

--- src/components/SkillsSection/SkillsSection.jsx
+++ src/components/SkillsSection/SkillsSection.jsx
@@ -6,13 +6,11 @@ function SkillsSection() {
    <div id="skills" className='subpage skills'>
         <h2>Skills</h2>
         <ul>
-          <li>C#<img height={14} width={18} src="icons/c-sharp.svg" alt="[c-sharp icon]" /></li>
-          <li>Python<img height={14} width={18} src="icons/python.svg" alt="[python icon]" /></li>
-          <li>Java<img height={14} width={18} src="icons/java.svg" alt="[java icon]"/></li>
-          <li>HTML5<img height={14} width={18} src="icons/html5.svg" alt="[html5 icon]" />
-            CSS3<img height={14} width={18} src="icons/css3.svg" alt="[css3 icon]" />
-            JavaScript<img height={14} width={18} src="icons/javascript.svg" alt="[javascript icon]" /></li>
-          <li>React<img height={12} width={18} src="icons/react.svg" alt="[react icon]" /></li>
+          <li><img height={14} width={18} src="icons/c-sharp.svg" alt="[c-sharp icon]" />C# & .NET (Xamarin, Forms)</li>
+          <li><img height={14} width={18} src="icons/python.svg" alt="[python icon]" />Python & ML (OpenCV, Scikit-learn, Tensorflow) </li>
+          <li><img height={14} width={18} src="icons/java.svg" alt="[java icon]"/>Java</li>                    
+          <li><img height={12} width={18} src="icons/react.svg" alt="[react icon]" />Front-end (ReactJS)</li>
+          <li>Containerization (Docker, Azure Kubernetes Service)</li>
         </ul>
    </div>
   )
